16184c51e570301e54e2810ba158abd8f41f94ad
Frederick Polgardy <fred@polgardy.com>
2019-06-09 16:11:58 -0600

    clean up variable declarations

--- src/core/rules.js
+++ src/core/rules.js
@@ -5,11 +5,11 @@ export default class Rules {
 	}
 
 	findJumps() {
-		let { board, side } = this,
-			top = side == 1 ? 7 : 0,
-			out = top + side,
-			bottom = top ^ 7,
-			jumps = [];
+		const { board, side } = this;
+		const top = side == 1 ? 7 : 0;
+		const out = top + side;
+		const bottom = top ^ 7;
+		const jumps = [];
 
 		// loop through playable squares
 		for (let y = bottom; y != out; y += side) {
@@ -29,39 +29,30 @@ export default class Rules {
 	}
 
 	nextJump(cur, jumps) {
-		let { board, side } = this,
-			[x, y] = cur[cur.length - 1],
-			p = board[y][x],
-			top = side == 1 ? 7 : 0,
-			king = p == side * 2,
-			found = false;
+		const { board, side } = this;
+		const [x, y] = cur[cur.length - 1];
+		const p = board[y][x];
+		const top = side == 1 ? 7 : 0;
+		const king = p == side * 2;
+		let found = false;
 
 		// loop over directions (dx, dy) from the current square
 		for (let dy = king ? -1 : 1; dy != 3; dy += 2) {
 			for (let dx = -1; dx != 3; dx += 2) {
-				let mx, my, nx, ny;
-
 				// calculate middle and landing coordinates
-				if (side == 1) {
-					mx = x + dx;
-					my = y + dy;
-					nx = mx + dx;
-					ny = my + dy;
-				} else {
-					mx = x - dx;
-					my = y - dy;
-					nx = mx - dx;
-					ny = my - dy;
-				}
+				const mx = x + (side == 1 ? dx : -dx);
+				const my = y + (side == 1 ? dy : -dy);
+				const nx = mx + (side == 1 ? dx : -dx);
+				const ny = my + (side == 1 ? dy : -dy);
 
 				// see if jump is on the board
 				if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
-					let m = board[my][mx],
-						n = board[ny][nx];
+					const m = board[my][mx];
+					const n = board[ny][nx];
 
 					// see if the middle piece is an opponent and the landing is open
 					if (n == 0 && side * m < 0) {
-						let crowned = !king && ny == top;
+						const crowned = !king && ny == top;
 						found = true;
 
 						// keep track of the coordinates, and move the piece
@@ -90,15 +81,39 @@ export default class Rules {
 		return found;
 	}
 
+	doJump(jump) {
+		const { board, side } = this;
+		const len = jump.length;
+		const [x, y] = jump[0];
+		const [fx, fy] = jump[len - 1];
+		const p = board[y][x];
+		const top = side == 1 ? 7 : 0;
+		const crowned = p == side && fy == top;
+
+		// remove the initial piece
+		board[y][x] = 0;
+
+		// loop over the passed in coords
+		for (let i = 1; i < len; ++i) {
+			const [, , mx, my] = jump[i];
+
+			// perform the jump
+			board[my][mx] = 0;
+		}
+
+		// final piece
+		board[fy][fx] = crowned ? p * 2 : p;
+	}
+
 	withJump(jump, action) {
-		let { board, side } = this,
-			len = jump.length,
-			[x, y] = jump[0],
-			[fx, fy] = jump[len - 1],
-			p = board[y][x],
-			top = side == 1 ? 7 : 0,
-			crowned = p == side && fy == top,
-			cap = new Array(len);
+		const { board, side } = this;
+		const len = jump.length;
+		const [x, y] = jump[0];
+		const [fx, fy] = jump[len - 1];
+		const p = board[y][x];
+		const top = side == 1 ? 7 : 0;
+		const crowned = p == side && fy == top;
+		const cap = new Array(len);
 
 		// remove the initial piece
 		cap[0] = p;
@@ -106,7 +121,7 @@ export default class Rules {
 
 		// loop over the passed in coords
 		for (let i = 1; i < len; ++i) {
-			let [, , mx, my] = jump[i];
+			const [, , mx, my] = jump[i];
 
 			// perform the jump
 			cap[i] = board[my][mx];
@@ -124,7 +139,7 @@ export default class Rules {
 
 		// loop over the passed in coords in reverse
 		for (let i = len - 1; i > 0; --i) {
-			let [, , mx, my] = jump[i];
+			const [, , mx, my] = jump[i];
 
 			// put back the captured piece
 			board[my][mx] = cap[i];
@@ -135,37 +150,30 @@ export default class Rules {
 	}
 
 	findMoves() {
-		let { board, side } = this,
-			top = side == 1 ? 7 : 0,
-			out = top + side,
-			bottom = top ^ 7,
-			moves = [];
+		const { board, side } = this;
+		const top = side == 1 ? 7 : 0;
+		const out = top + side;
+		const bottom = top ^ 7;
+		const moves = [];
 
 		// loop through playable squares
 		for (let y = bottom; y != out; y += side) {
 			for (let x = bottom; x != out; x += side) {
-				let p = board[y][x],
-					king = p == side * 2;
+				const p = board[y][x];
+				const king = p == side * 2;
 
 				// see if it's our piece
 				if (side * p > 0) {
 					// loop over directions (dx, dy) from the current square
 					for (let dy = king ? -1 : 1; dy != 3; dy += 2) {
 						for (let dx = -1; dx != 3; dx += 2) {
-							let nx, ny;
-
 							// calculate landing coordinates
-							if (side == 1) {
-								nx = x + dx;
-								ny = y + dy;
-							} else {
-								nx = x - dx;
-								ny = y - dy;
-							}
+							const nx = x + (side == 1 ? dx : -dx);
+							const ny = y + (side == 1 ? dy : -dy);
 
 							// see if move is on the board
 							if (nx >= 0 && nx < 8 && ny >= 0 && ny < 8) {
-								let crowned = !king && ny == top;
+								const crowned = !king && ny == top;
 
 								// see if the landing is open
 								if (board[ny][nx] == 0) {
@@ -189,14 +197,26 @@ export default class Rules {
 		return moves;
 	}
 
-	withMove(move, action) {
-		let { board, side } = this,
-			[[x, y], [nx, ny]] = move,
-			p = board[y][x],
-			top = side == 1 ? 7 : 0,
-			crowned = p == side && ny == top;
+	doMove(move) {
+		const { board, side } = this;
+		const [[x, y], [nx, ny]] = move;
+		const p = board[y][x];
+		const top = side == 1 ? 7 : 0;
+		const crowned = p == side && ny == top;
 
-		// perform the jump
+		// perform the move
+		board[y][x] = 0;
+		board[ny][nx] = crowned ? p * 2 : p;
+	}
+
+	withMove(move, action) {
+		const { board, side } = this;
+		const [[x, y], [nx, ny]] = move;
+		const p = board[y][x];
+		const top = side == 1 ? 7 : 0;
+		const crowned = p == side && ny == top;
+
+		// perform the move
 		board[y][x] = 0;
 		board[ny][nx] = crowned ? p * 2 : p;
 
@@ -209,7 +229,7 @@ export default class Rules {
 	}
 
 	findPlays() {
-		let jumps = this.findJumps();
+		const jumps = this.findJumps();
 
 		// you have to jump if you can
 		if (jumps.length) {
@@ -220,12 +240,12 @@ export default class Rules {
 	}
 
 	buildTree() {
-		let plays = this.findPlays(),
-			tree = {};
+		const plays = this.findPlays();
+		const tree = {};
 
 		for (let i = 0; i < plays.length; ++i) {
-			let play = plays[i],
-				root = tree;
+			const play = plays[i];
+			let root = tree;
 
 			for (let j = 0; j < play.length; ++j) {
 				let [x, y] = play[j],
